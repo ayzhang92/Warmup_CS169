6b470434fc7d5b4ac74c5ae0939bdaef3c66db5f
Amy Zhang <amyzhang92@berkeley.edu>
2013-02-14 00:29:58 -0800

    why is testing harder to write than the actual app?

--- app/controllers/testapi_controller.rb
+++ app/controllers/testapi_controller.rb
@@ -1,5 +1,4 @@
 class TestapiController < ApplicationController
-  # add resetFixture and unitTests functions here??
   def resetFixture
     User.delete_all()
     render :json => {:errCode => 1}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,5 +30,4 @@ class UsersController < ApplicationController
     end
   end
 
-
 end
